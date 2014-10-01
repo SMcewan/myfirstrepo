0c916a21a5c999760482f8c3b6fb26ddd4746271
unknown <m008059c@k106-10.student.staffs.ac.uk>
2014-10-01 11:29:40 +0100

    Player Class added

---
+++ tut_proj_1/tut_proj_1/player.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace tut_proj_1
+{
+	public class player
+	{
+		public player ()
+		{
+			int health = 0;
+			int strenght = 10;
+			string name = "Dave the Dangerous";
+			int luck = -50;
+			
+			
+			
+		}
+		
+		public int Attack ()
+		{
+			
+			return attack;
+		}
+		
+		public string NinjaEncounter()
+		{
+			return ShurikenToFace;	
+		}
+	}
+}
+
